f9cd968a471d3066ab06ce66c48c8a257dfcdc02
Doriano <doriano.bergamin18@gmail.com>
2024-01-23 19:25:46 +1100

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  const scuberDrivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+  const firstTwoDrivers = returnFirstTwoDrivers(scuberDrivers);
+
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(2, 4);
+  };
+  
+  const scuberDriver = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+  const lastTwoDrivers = returnLastTwoDrivers(scuberDriver);
+
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  const createFareMultiplier = function(integer){
+        return function(fare){
+            return integer * fare
+}
+  }
+
+  const fareDoubler = createFareMultiplier(2)
+
+  const fareTripler = createFareMultiplier(3)
+
+  function selectDifferentDrivers(drivers, driversSelector){
+    return driversSelector(drivers)
+  }
+
+  selectDifferentDrivers(drivers, returnLastTwoDrivers)
